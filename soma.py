defe7ab97e052f835e017f391923396ebc10d89b
KevinAFS <kevinafsilva10@gmail.com>
2025-06-10 22:00:30 -0300

    Update soma(Kevin).py

--- soma.py
+++ soma.py
@@ -1,2 +1,11 @@
-def soma(e,b):
+import random
+
+def soma(e, b):
     return e+b
+
+a = random.randint(1, 100)
+b = random.randint(1, 100)
+
+resultado = soma(a,b)
+print (f"Os números sorteados foram {a} e {b}" )
+print (f"A soma desses mesmos números é {resultado}")
